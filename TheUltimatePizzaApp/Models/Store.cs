f45253ef20fea6aafccbf1bd7fbff3858ab16f61
jvillad <jyvillad@gmail.com>
2023-02-27 01:11:29 +1100

    added a model for stores

---
+++ TheUltimatePizzaApp/Models/Store.cs
@@ -0,0 +1,9 @@
+﻿namespace TheUltimatePizzaApp.Model
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
