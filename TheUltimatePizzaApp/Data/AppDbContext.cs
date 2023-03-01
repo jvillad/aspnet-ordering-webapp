66b7a4a3fcca7b152a51e332ff01a3d727267507
jvillad <jyvillad@gmail.com>
2023-03-02 01:16:08 +1100

    added Application DBContext

---
+++ TheUltimatePizzaApp/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TheUltimatePizzaApp.Model;
+
+namespace TheUltimatePizzaApp.Data
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Pizza> Pizzas { get; set; }
+	}
+}
