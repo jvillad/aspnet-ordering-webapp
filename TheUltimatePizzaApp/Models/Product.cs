4db6cd865f52f44d1ff064b4ddb01030117f6492
jvillad <jyvillad@gmail.com>
2023-03-05 22:50:10 +0900

    product properties

---
+++ TheUltimatePizzaApp/Models/Product.cs
@@ -0,0 +1,22 @@
+namespace TheUltimatePizzaApp.Model;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string ShortDescription { get; set; }
+
+    public string Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string Category { get; set; }
+
+    public IFormFile Upload { get; set; }
+
+    public string ImageFileName { get; set; }
+
+    public DateTime Created { get; set; }
+}
